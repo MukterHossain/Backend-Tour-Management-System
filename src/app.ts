087cc60b637572314ac0ec9547312544f1e76b1c
MukterHossain <mukter3075@gmail.com>
2025-07-09 11:07:45 +0600

    added error handle and some methods

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express, { Request, Response } from 'express';
 import cors from "cors"
 import { router } from './app/routes';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 
 const app = express()
@@ -23,7 +24,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use(globalErrorHandler)
 
-
+app.use(notFound)
 
 
 
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,26 +1,85 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes"
 import { userService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+// import AppError from "../../errorHelpers/AppError";
+
+// type AsyncHandler = (req:Request, res:Response, next:NextFunction) => Promise<void>
+
+// const catchAsync = (fn: AsyncHandler) => (req:Request, res:Response, next:NextFunction) => {
+//     Promise.resolve(fn(req, res, next)).catch((err: any) =>{
+//         console.log(err)
+//         next(err)
+//     })
+// }
 
 
-
-const createUser = async (req:Request, res:Response, next:NextFunction) => {
-    try {
-        const user = await userService.createUser(req.body)
-        res.status(httpStatus.CREATED).json({
+// const createUser = async (req:Request, res:Response, next:NextFunction) => {
+//     try {
+//         // throw new Error("Fack error")
+//         // throw new AppError(httpStatus.BAD_REQUEST, "Fack Error")
+//         const user = await userService.createUser(req.body)
+//         res.status(httpStatus.CREATED).json({
+//             message: "User Created Successfully",
+//             user
+//         })
+//     } catch (err:any) {
+//         console.log(err)
+//         next(err)
+//     }
+// }
+const createUser = catchAsync(async (req:Request, res:Response, next:NextFunction) => {
+    const user = await userService.createUser(req.body)
+        // res.status(httpStatus.CREATED).json({
+        //     message: "User Created Successfully",
+        //     user
+        // })
+        sendResponse(res, {
+            success: true,
+            statusCode: httpStatus.CREATED,
             message: "User Created Successfully",
-            user
+            data: user
         })
-    } catch (err:any) {
-        console.log(err)
-        next(err)
-    }
-}
+})
+
+
+// const getAllUsers = async (req:Request, res:Response, next:NextFunction) => {
+//     try {
+//         const users = await userService.getAllUsers()
+//         return users
+//         // res.status(httpStatus.CREATED).json({
+//         //     message: "User Created Successfully",
+//         //     users
+//         // })
+//     } catch (err:any) {
+//         console.log(err)
+//         next(err)
+//     }
+// }
+
+const getAllUsers = catchAsync(async (req:Request, res:Response, next:NextFunction) => {
+    const result = await userService.getAllUsers()
+        // res.status(httpStatus.OK).json({
+        //     success: true,
+        //     message: "All Users Retrived Successfully",
+        //     users
+        // })
+        sendResponse(res, {
+            success: true,
+            statusCode: httpStatus.CREATED,
+            message: "All Users Retrived Successfully",
+            data: result.data,
+            meta: result.meta
+        })
+})
 
 
 
 export const UserControllers = {
-    createUser
+    createUser,
+    getAllUsers
 }
