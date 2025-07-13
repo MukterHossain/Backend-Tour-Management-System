c46c571b4ea06c15c18822deb5303b32717f23bd
MukterHossain <mukter3075@gmail.com>
2025-07-13 15:00:58 +0600

    added some logic and route

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,10 +4,25 @@ import { sendResponse } from "../../utils/sendResponse"
 import { catchAsync } from "../../utils/catchAsync"
 import httpStatus from "http-status-codes"
 import { AuthServices } from "./auth.service"
+import AppError from "../../errorHelpers/AppError"
+import { setAuthCookie } from "../../utils/setCookies"
 
 const credentialsLogin = catchAsync(async (req:Request, res:Response, next:NextFunction) => {
-    
+        
         const loginInfo = await AuthServices.credentialsLogin(req.body)
+
+        // res.cookie("accessToken", loginInfo.accessToken, {
+        //     httpOnly: true,
+        //     secure: false
+        // })
+
+        setAuthCookie(res, loginInfo)
+        
+        res.cookie("refreshToken", loginInfo.refreshToken, {
+            httpOnly: true,
+            secure: false
+        })
+
         sendResponse(res, {
             success: true,
             statusCode: httpStatus.CREATED,
@@ -16,8 +31,16 @@ const credentialsLogin = catchAsync(async (req:Request, res:Response, next:NextF
         })
 })
 const getNewAccessToken = catchAsync(async (req:Request, res:Response, next:NextFunction) => {
-        
-        const tokenInfo = await AuthServices.credentialsLogin(req.body)
+        const refreshToken = req.cookies.refreshToken
+        // const refreshToken = req.headers.authorization
+        if(!refreshToken){
+            throw new AppError(httpStatus.BAD_REQUEST, "No refresh token recieved from cookies")
+        }
+        const tokenInfo = await AuthServices.getNewAccessToken(refreshToken as string)
+        res.cookie("accessToken", tokenInfo.accessToken, {
+            httpOnly: true,
+            secure: false
+        })
         sendResponse(res, {
             success: true,
             statusCode: httpStatus.CREATED,
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,48 +3,57 @@ import { IUser } from "../user/user.interface"
 import { User } from "../user/user.model"
 import httpStatus from "http-status-codes"
 import bcryptjs from "bcryptjs"
-// import jwt from "jsonwebtoken"
-import { generateToken } from "../../utils/jwt"
-import { envVars } from "../../config/env"
+import { createNewAccessTokenWithRefreshToken, createUserToken } from "../../utils/userTokens"
 
 
-const credentialsLogin = async (payload: Partial<IUser>) =>{
-const {email, password} = payload
 
-const isUserExist = await User.findOne({email})
-    if(!isUserExist){
+
+const credentialsLogin = async (payload: Partial<IUser>) => {
+    const { email, password } = payload
+
+    const isUserExist = await User.findOne({ email })
+    if (!isUserExist) {
         throw new AppError(httpStatus.BAD_REQUEST, "User Email does not Exist")
     }
 
     const isPasswordMatched = await bcryptjs.compare(password as string, isUserExist.password as string)
-    if(!isPasswordMatched){
+    if (!isPasswordMatched) {
         throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password")
     }
-    const jwtPayload = {
-        userId: isUserExist._id,
-        email: isUserExist.email,
-        role: isUserExist.role
-    }
-    // const accessToken = jwt.sign(jwtPayload, 'secret', {
-    //     expiresIn: '30d'
-    // })
-    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRE)
+    // const jwtPayload = {
+    //     userId: isUserExist._id,
+    //     email: isUserExist.email,
+    //     role: isUserExist.role
+    // }
+    // // const accessToken = jwt.sign(jwtPayload, 'secret', {
+    // //     expiresIn: '30d'
+    // // })
+    // const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRE)
 
-    const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES)
+    // const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES)
+
+    const userTokens = createUserToken(isUserExist)
 
     // delete isUserExist.password;
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const {password : PASS, ...rest} = isUserExist.toObject()
+    const { password: PASS, ...rest } = isUserExist.toObject()
     return {
-        accessToken,
-        refreshToken,
+        accessToken: userTokens.accessToken,
+        refreshToken: userTokens.refreshToken,
         user: rest
         // email: isUserExist.email
     }
 }
+const getNewAccessToken = async (refreshToken: string) => {
+    const newAccessToken = createNewAccessTokenWithRefreshToken(refreshToken)
+    return {
+        accessToken: newAccessToken
+    }
+}
 
 
-export const AuthServices ={
+export const AuthServices = {
     credentialsLogin,
-    
+    getNewAccessToken
+
 }
