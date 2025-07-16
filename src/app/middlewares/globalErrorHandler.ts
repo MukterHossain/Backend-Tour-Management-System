8ba33608347139fbe1b5c10fe25abf38043b70b1
MukterHossain <mukter3075@gmail.com>
2025-07-16 15:10:49 +0600

    added tour and division section

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,18 +3,20 @@
 import { NextFunction, Request, Response } from "express"
 import { envVars } from "../config/env"
 import AppError from "../errorHelpers/AppError";
+import mongoose from "mongoose";
+import { TErrorSources, TGenericErrorResponse } from "../interfaces/error.types";
+import { handleDuplicateError } from "../helpers/handleDuplicateError";
+import { handleCastError } from "../helpers/handleCastError";
+import { handleZodError } from "../helpers/handleZodError";
+import { handleValidationError } from "../helpers/handleValidationError";
 
 
 
-const handleDuplicateError =(err: any) =>{
-    const matchArray = err.message.match(/"([^"]*)"/)
-        return {
-            statusCode: 400,
-        message:`${matchArray[1]} already exists!!`
-        }
-}
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    console.log(err)
+    if(envVars.NODE_ENV === "development"){
+        console.log(err)
+    }
+    
     // const statusCode = 500;
     // const message = `Something went Wrong!! ${err.message}`
     let statusCode = 500;
@@ -26,7 +28,7 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
      * - cast error
      */
 
-    const errorSources :any =[]
+    let errorSources: TErrorSources[] = []
     // Duplicate Error
     // if(err.code === 11000){
     //     console.log("Duplicate error")
@@ -34,56 +36,75 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
     //     statusCode= 400;
     //     message= `${matchArray[1]} already exists!!`
     // }
-    if(err.code === 11000){
+    if (err.code === 11000) {
         const simplifiedError = handleDuplicateError(err)
-        statusCode= simplifiedError.statusCode;
-        message= `${matchArray[1]} already exists!!`
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message
     }
     // Object Id error / cast Error
-    else if(err.name === "CastError"){
-        statusCode = 400;
-        message= "Invalid MongoDB ObjectId. Please valid Id"
+    // else if(err.name === "CastError"){
+    //     statusCode = 400;
+    //     message= "Invalid MongoDB ObjectId. Please valid Id"
+    // }
+    else if (err.name === "CastError") {
+        const simplifiedError = handleCastError(err)
+        statusCode = simplifiedError.statusCode
+        message = simplifiedError.message
     }
 
-    else if(err.name === "ZodError"){
-        statusCode = 400
-        message= "Zod Error"
+    // Zod error
+    // else if (err.name === "ZodError") {
+    //     statusCode = 400
+    //     message = "Zod Error"
 
-        console.log(err.issues)
-        err.issues.forEach((issue: any) => {
-            errorSources.push({
-                path: issue.path[issue.path.length - 1],
-                message: issue.message
-            })
-        })
+    //     console.log(err.issues)
+    //     err.issues.forEach((issue: any) => {
+    //         errorSources.push({
+    //             path: issue.path[issue.path.length - 1],
+    //             message: issue.message
+    //         })
+    //     })
+    // }
+    else if (err.name === "ZodError") {
+        const simplifiedError = handleZodError(err)
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources as TErrorSources[]
     }
 
     // Mongoose validation Error
-    else if(err.name === "ValidationError"){
-        statusCode = 400;
-        const errors = Object.values(err.errors)
-        
-        errors.forEach((errorObject:any) => errorSources.push({
-            path: errorObject.path,
-            message: errorObject.message
-        }))
-        // message= err.message
-        message= "Validation Error"
+    // else if (err.name === "ValidationError") {
+    //     statusCode = 400;
+    //     const errors = Object.values(err.errors)
+
+    //     errors.forEach((errorObject: any) => errorSources.push({
+    //         path: errorObject.path,
+    //         message: errorObject.message
+    //     }))
+    //     // message= err.message
+    //     message = "Validation Error"
+    // }
+    else if (err.name === "ValidationError") {
+        const simplifiedError = handleValidationError(err)
+        statusCode = simplifiedError.statusCode
+        errorSources = simplifiedError.errorSources as TErrorSources[]
+        message = simplifiedError.message
     }
 
-    else if(err instanceof AppError){
+
+    else if (err instanceof AppError) {
         statusCode = err.statusCode
         message = err.message
     }
-    else if(err instanceof Error){
+    else if (err instanceof Error) {
         statusCode = 500
         message = err.message
     }
     res.status(500).json({
-        success:false,
+        success: false,
         message,
         errorSources,
-        err,
+        err: envVars.NODE_ENV === "development" ? err : null,
         stack: envVars.NODE_ENV === "development" ? err.stack : null
     })
 }
--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 
 
 
+
 type AsyncHandler = (req:Request, res:Response, next:NextFunction) => Promise<void>
 
 
