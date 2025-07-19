e391ee4a02b495c77995a94b4e83505fc9575f48
MukterHossain <mukter3075@gmail.com>
2025-07-19 16:08:45 +0600

    added some fixed

--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -1,3 +1,4 @@
+// import { excludeField } from "../../constants";
 import { QueryBuilder } from "../../utils/QueryBuilder";
 import { tourSearchableFields } from "./tour.constant";
 import { ITour, ITourType } from "./tour.interface";
@@ -77,45 +78,83 @@ const createTour = async (payload: ITour) => {
 //     // search = Golf
 //     const totalTours = await Tour.countDocuments();
 //     // const totalPage = 21/10 = 2.1 => ciel(2.1) => 3
-//     const totalPage = Math.ceil(totalTours / limit)
+    // const totalPage = Math.ceil(totalTours / limit)
 
-//     const meta = {
-//         page: page,
-//         limit: limit,
-//         total: totalTours,
-//         totalPage: totalPage,
-//     }
-//     return {
-//         data: allTours,
-//         meta: meta
-//     }
+    // const meta = {
+    //     page: page,
+    //     limit: limit,
+    //     total: totalTours,
+    //     totalPage: totalPage,
+    // }
+    // return {
+    //     data: allTours,
+    //     meta: meta
+    // }
 // }
 
 
 
 const getAllTours = async (query: Record<string, string>) => {
-    // const queryBuilder = new QueryBuilder(Tour.find(), query)
-    const tours = await Tour.find({})
-    const totalTours = await Tour.countDocuments()
-    // const tours = await queryBuilder.search(tourSearchableFields).filter().sort().fields().paginate()
-    // // const meta = await queryBuilder.getMeta()
-    // const [data, meta] = await Promise.all([
-    //     tours.build(),
-    //     queryBuilder.getMeta()
-    // ])
+    
+    // console.log(query)
+    // const filter = query;
+    // const searchTerm = query.searchTerm || "";
+    // const sort = query.sort || "-createdAt";
+    // const page = Number(query.page) || 1
+    // const limit = Number(query.limit) || 10
+    // const skip = (page -1) * limit;
+    // // delete filter["searchTerm"]
+    //         for (const field of excludeField) {
+    //     // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+    //     delete filter[field]
+    // }
+    // const fields = query.fields?.split(",").join(" ") ||  ""
+    // console.log("Slected fields", fields)
+    //  const searchQuery = {
+    //        $or: tourSearchableFields.map(field => ({[field]: {$regex:searchTerm, $options: "i"}})) 
+    //     }
+    // const tours = await Tour.find(searchQuery).find(filter).sort(sort).select(fields).skip(skip).limit(limit)
+       
+    //     // title: {$regex:searchTerm, $options: "i"}
+    //     // $or:[
+    //     //     {title: {$regex:searchTerm, $options: "i"}},
+    //     //     {description: {$regex:searchTerm, $options: "i"}},
+    //     //     {location: {$regex:searchTerm, $options: "i"}},
+    //     // ]
+    
+
+    // const totalTours = await Tour.countDocuments()
+    // //     const totalTours = await Tour.countDocuments();
+    //     const totalPage = Math.ceil(totalTours / limit)
+
+    // const meta = {
+    //     page: page,
+    //     limit: limit,
+    //     total: totalTours,
+    //     totalPage: totalPage,
+    // }
+    // return {
+    //     data:tours,
+    //     meta: meta
+    //     }
+    
+
+
+
+    const queryBuilder = new QueryBuilder(Tour.find(), query)
+    const tours = queryBuilder.search(tourSearchableFields).filter().sort().fields().paginate()
+    // const meta = await queryBuilder.getMeta()
+    const [data, meta] = await Promise.all([
+        tours.build(),
+        queryBuilder.getMeta()
+    ])
+
+
 
     return {
-        data: tours,
-        meta: {
-            total: totalTours
-        }
+        data,
+        meta
     }
-
-
-    // return {
-    //     data,
-    //     meta
-    // }
 }
 
 
